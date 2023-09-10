9dcd4bf15e1501e8ecc60aaf2d50d62a90464ebf
Miguel Figueroa Perez <142467622+DonFigue@users.noreply.github.com>
2023-09-09 23:16:02 -0400

    Clases
    Clases de la V1 de la BDD

---
+++ Clases/InstitutionADO/InstitutionADO/Model/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InstitutionADO.Model
+{
+    internal class User : Person
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Rol { get; set; }
+
+        public User()
+        {
+            
+        }
+    }
+}
